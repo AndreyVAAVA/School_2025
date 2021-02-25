77ee882c64eb84eb36307fa3b6cecaa018b0f4e3
Andrey Vokov <ballgameandrew@yandex.ru>
2021-02-26 02:00:53 +0300

    HomeWork_Number26

--- main.py
+++ main.py
@@ -1,46 +1,32 @@
-#Найдите последовательность повторяющихся символов (к примеру AAAA, BBBbbB) и запишите в строку, и если после символов будут цифры, то найдите и запишите их в строку, где были до этого символы(добавьте к тому), если конечно цифры после последовательности есть.
-#Помимо этого если будут цифры, то найдите сумму всех цифр(не чисел(для AAAA12372 вы должны сделать так 1+2+3+7+2 = 15)) и выведите.
-#Запишите длину получившейся строки и выведите.
-def check(number):
-    try:
-        int(number)
-        return True
-    except Exception:
-        return False
-f = open("24-3.txt", "r")
-connctn = f.readline()
-maxlen = 1
-minlen = 0
-data = ""
-data_2 = ""
-size = len(connctn) - 1
-sum = 0
-for i in range(150, size):
-    smthng = connctn[i]
-    number_1 = connctn[i]
-    number_2 = connctn[i + 1]
-    number_prev = connctn[i - 1]
-    if number_1.lower() == number_2.lower() and i != size:
-        minlen += 1
-        data_2 = data_2 + smthng
-    elif number_prev == number_1:
-        minlen += 1
-        data_2 = data_2 + smthng
-    elif data_2 != "" and check(number_1) == True:
-        data_2 = data_2 + number_1
-    else:
-        if maxlen < minlen:
-            maxlen = minlen
-            minlen = 0
-            data = data_2
-            data_2 = ""
-        else:
-            minlen = 0
-            data_2 = ""
-f.close()
-for i in data:
-    if check(i) == True:
-        sum += int(i)
-print(maxlen)
-print(data)
-print(sum)
+lst = open("26-k6.txt").readlines()
+lst = [[int(n) for n in x.split()] for x in lst]
+print(lst)
+lst = sorted(lst)
+print(lst)
+lst.sort(key=lambda x: x[1])
+print(lst)
+m = int(input("Максимальный бюджет (как бы в задаче очень плохо написано требование вот и выдумал его в виде максимальной суммы, которую вводит пользователь, а ещё там задача имеет кое-какое противоречие): "))
+total = 0
+total_weight = 0
+weight_max = 0
+perm1 = 0
+perm2 = 0
+price = 0
+prev = 0
+cont = 0
+while total <= m:
+    perm1 = lst[0][1]
+    perm2 = lst[0][0]
+    total += (perm1 * perm2)
+    total_weight += perm2
+    if weight_max < perm2 and total <= m:
+        prev = weight_max
+        cont = price
+        weight_max = perm2
+        price = perm1
+    lst.remove(lst[0])
+total -= (perm1 * perm2)
+total_weight -= perm2
+print(total)
+print("Answer:")
+print(total_weight, price)
